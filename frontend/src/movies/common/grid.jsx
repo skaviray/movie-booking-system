52fbc2294882a9cc4d9e2b7a6f19f209b341af9b
Shiva Prasad Kavirayani <kavirayani1991@gmail.com>
2025-07-21 18:49:06 +0530

    Minor modifications to grid

--- frontend/src/movies/common/grid.jsx
+++ frontend/src/movies/common/grid.jsx
@@ -13,13 +13,13 @@ export default function MoviesGridView({user,movies, onBook,onDelete, onLike}) {
         {movies.map(movie => (
         <div className="col-sm-6 col-md-4 col-lg-3 mb-4" key={movie.id}>
             <div className="card">
-            <img src={movie.poster} className="card-img-top" alt={movie.title} style={{ height: '400px', objectFit: 'cover' }}/>
-            <div className="card-body">
-                {renderCell(movie)}
-                <p className="card-text">{movie.genre}</p>
+            <img src={movie.poster} className="card-img-top" alt={movie.title} style={{ height: '400px', objectFit: 'cover' }} onClick={() => onBook(movie)}/>
+            <div className="card-body d-flex flex-column">
+                <p>{movie.title}</p>
                 <Like liked={movie.liked} onClick={() => onLike(movie)}/>
-                <button onClick={() => onBook(movie)  } className="btn btn-primary btn-sm">Book</button>
+                <div className="mt-auto d-flex justify-content-end gap-2">
                 {user && user.is_admin && <button onClick={() => onDelete(movie)  } className="btn btn-danger btn-sm">Delete</button>}
+                </div>
             </div>
             </div>
         </div>
